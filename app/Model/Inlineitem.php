e7f76d08efecb2e9969ec1100cc9024b2097a207
ajay <ajayp944@gmail.com>
2014-10-09 17:02:54 +0530

    Create Model

---
+++ app/Model/Inlineitem.php
@@ -0,0 +1,10 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Inlineitem Model
+ *
+ */
+class Inlineitem extends AppModel {
+
+
+}
---
+++ app/Model/Order.php
@@ -0,0 +1,10 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Order Model
+ *
+ */
+class Order extends AppModel {
+
+
+}
