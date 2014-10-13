bb952688cc6a76c3b18cd20b2dfd2b4a1d6fe20a
himanverma <himan.verma@live.com>
2014-10-13 18:35:19 +0530

    Added new Binding for Color Swatch

--- app/webroot/js/app/custom.bindings.js
+++ app/webroot/js/app/custom.bindings.js
@@ -5,44 +5,72 @@
  * <span data-bind="cm2ft2cm:{'ft':12,'unit':mUnits}">12</span>ft
  */
 ko.bindingHandlers.cm2ft2cm = {
-     init: function (element, valueAccessor, allBindingsAccessor) {
+    init: function(element, valueAccessor, allBindingsAccessor) {
         // First get the latest data that we're bound to
         var value = valueAccessor();
-        if(value.cm != undefined){
-            if(value.unit() == 'cm'){
+        if (value.cm != undefined) {
+            if (value.unit() == 'cm') {
                 $(element).html(value.cm);
             }
-            if(value.unit() == 'ft'){
+            if (value.unit() == 'ft') {
                 $(element).html(value.cm * 0.032808);
             }
         }
-        if(value.ft != undefined){
-            if(value.unit() == 'ft'){
+        if (value.ft != undefined) {
+            if (value.unit() == 'ft') {
                 $(element).html(value.ft);
             }
-            if(value.unit() == 'cm'){
+            if (value.unit() == 'cm') {
                 $(element).html(value.ft / 0.032808);
             }
         }
     },
-    update: function (element, valueAccessor, allBindingsAccessor) {
+    update: function(element, valueAccessor, allBindingsAccessor) {
         // First get the latest data that we're bound to
         var value = valueAccessor();
-        if(value.cm != undefined){
-            if(value.unit() == 'cm'){
+        if (value.cm != undefined) {
+            if (value.unit() == 'cm') {
                 $(element).html((value.cm).toFixed(2));
             }
-            if(value.unit() == 'ft'){
+            if (value.unit() == 'ft') {
                 $(element).html((value.cm * 0.032808).toFixed(2));
             }
         }
-        if(value.ft != undefined){
-            if(value.unit() == 'ft'){
+        if (value.ft != undefined) {
+            if (value.unit() == 'ft') {
                 $(element).html((value.ft).toFixed(2));
             }
-            if(value.unit() == 'cm'){
+            if (value.unit() == 'cm') {
                 $(element).html((value.ft / 0.032808).toFixed(2));
             }
-        }       
+        }
+    }
+};
+ko.bindingHandlers.swatch = {
+    init: function(element, valueAccessor, allBindingsAccessor) {
+        // First get the latest data that we're bound to
+        var value = valueAccessor();
+        if (value.clrstamp != undefined) {
+            var a = value.clrstamp.toString();
+            var b = a.split("-");
+            var c = "";
+            for (i in b) {
+                c += '<img src="/swatch/' + b[i] + '.png" />';
+            }
+            $(element).html(c);
+        }
+    },
+    update: function(element, valueAccessor, allBindingsAccessor) {
+        // First get the latest data that we're bound to
+        var value = valueAccessor();
+        if (value.clrstamp != undefined) {
+            var a = value.clrstamp.toString();
+            var b = a.split("-");
+            var c = "";
+            for (i in b) {
+                c += '<img src="/swatch/' + b[i] + '.png" />';
+            }
+            $(element).html(c);
+        }
     }
 };
